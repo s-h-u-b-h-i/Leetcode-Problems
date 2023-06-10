3da87c699aa6d80f7f75b84030a7a86dff634d93
Shubhi Sharma <82764814+s-h-u-b-h-i@users.noreply.github.com>
2023-06-10 23:00:55 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int arr[], int n)
+    {
+        int temp[] = new int[n];
+        int k = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] < 0) {
+                temp[k++] = arr[i];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(arr[i] >= 0) {
+                temp[k++] = arr[i];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+}
