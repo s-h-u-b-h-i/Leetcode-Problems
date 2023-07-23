fd594d838163b4365df447ba6d10b883312c7c2b
Shubhi Sharma <82764814+s-h-u-b-h-i@users.noreply.github.com>
2023-07-23 21:42:07 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        int z=0,o=0,t=0;
+        Node p=head;
+        while(p!=null){
+            if(p.data==0) z++;
+            else if(p.data==1) o++;
+            else t++;
+            p=p.next;
+        }
+        p=head;
+        int c=1;
+        while(p!=null){
+            if(c<=z) p.data=0;
+            else if(c<=o+z) p.data=1;
+            else p.data=2;
+            p=p.next;
+            c++;
+        }
+        return head;
+    }
+}
+
+
