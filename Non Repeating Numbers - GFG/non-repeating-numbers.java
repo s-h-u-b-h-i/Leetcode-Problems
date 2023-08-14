12f012726eaa61093f0b247dc23456199db5cfd7
Shubhi Sharma <82764814+s-h-u-b-h-i@users.noreply.github.com>
2023-08-14 18:21:03 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        int[] a=new int[2];int c=0;
+        int n=(nums.length-2)/2;
+        Arrays.sort(nums);
+        if(nums[2*n+2-1]!=nums[2*n]) a[1]=nums[2*n+1];
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]!=nums[i+1]) {a[c]=nums[i];c++;}
+            else i++;
+        }
+        return a;
+    }
+}
